77ab1b1a4f8422e0eb95ee5f660d51032be139da
Bryce Engberg <bryceengberg@gmail.com>
2021-05-04 13:49:15 -0500

    j

--- 2021_parradeRobot/src/main/java/frc/robot/Robot.java
+++ 2021_parradeRobot/src/main/java/frc/robot/Robot.java
@@ -9,10 +9,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.SPI;
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -28,40 +24,32 @@ public class Robot extends TimedRobot {
 
   XboxController Xbox = new XboxController(0);
 
-  Double btn_DriveFB = Xbox.getRawAxis(0);
-  Double btn_DriveSpin = Xbox.getRawAxis(1);
-
-  Gyro gyro = new ADXRS450_Gyro(SPI.Port.kMXP);
-  Double gyroAngle = gyro.getAngle();
+  Double btn_DriveFB = Xbox.getRawAxis(5);
+  Double btn_DriveSpin = Xbox.getRawAxis(4);
 
   @Override
-  public void robotInit() {
-    gyro.calibrate();
-  }
+  public void robotInit() { }
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {
-
-  }
+  public void autonomousInit() {}
 
   @Override
-  public void autonomousPeriodic() {
-  }
+  public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {
-  }
+  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {
 
-    btn_DriveFB = Xbox.getRawAxis(0);
-    btn_DriveSpin = Xbox.getRawAxis(1);
+    btn_DriveFB = Xbox.getRawAxis(5);
+    btn_DriveSpin = Xbox.getRawAxis(4);
 
-    drive.arcadeDrive(0.8 * btn_DriveFB, 0.8 * btn_DriveSpin);
+
+    drive.arcadeDrive(-0.8*btn_DriveFB, 0.8*btn_DriveSpin);
   }
 
   @Override
